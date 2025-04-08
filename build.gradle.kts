a2fda1e029a0bb12bb7a0b299d658d20cdde6b3a
Hknots <hknots99@gmail.com>
2025-04-08 10:25:42 +0200

    build: add mavenLocal repository and core-resource-server dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,11 +15,13 @@ java {
 }
 
 repositories {
+    mavenLocal()
     mavenCentral()
 }
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("no.fintlabs:core-resource-server:0.0.1-SNAPSHOT")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
